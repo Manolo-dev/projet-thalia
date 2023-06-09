9be7eed2ba90ebe864774e3bca3881d8eded69d5
Manolo-dev <sardo.manolo.dev@gmail.com>
2023-06-09 07:59:47 +0200

    images

--- src/scripts/slide.js
+++ src/scripts/slide.js
@@ -1,87 +1,91 @@
-const slideContainer = document.querySelector('.slide');
-const slideWrapper = document.querySelector('.slide-wrapper');
-const slides = Array.from(document.querySelectorAll('.slide-item'));
-let touchstartX = 0;
-let touchstartY = 0;
-let touchendX = 0;
-let touchendY = 0;
-let isSlidingWithScroll = false;
-let currentSlide = 0;
+// Sélectionnez tous les conteneurs des sliders
+const slideContainers = Array.from(document.querySelectorAll('.slide'));
 
-function goToSlide(index) {
-    currentSlide = index;
-    const slideWidth = slides[0].offsetWidth;
-    const newPosition = -1 * slideWidth * index;
-    slideWrapper.style.transform = `translateX(${newPosition}px)`;
-    document.querySelector('.slide-buttons-item.active').classList.remove('active');
-    document.querySelectorAll('.slide-buttons-item')[currentSlide].classList.add('active');
-}
+slideContainers.forEach((slideContainer) => {
+    const slideWrapper = slideContainer.querySelector('.slide-wrapper');
+    const slides = Array.from(slideContainer.querySelectorAll('.slide-item'));
+    let touchstartX = 0;
+    let touchstartY = 0;
+    let touchendX = 0;
+    let touchendY = 0;
+    let isSlidingWithScroll = false;
+    let currentSlide = 0;
 
-function swipeleft() {
-    console.log(slides.length);
-    if (currentSlide < slides.length - 1) {
-        goToSlide(currentSlide + 1);
+    function goToSlide(index) {
+        currentSlide = index;
+        const slideWidth = slides[0].offsetWidth;
+        const newPosition = -1 * slideWidth * index;
+        slideWrapper.style.transform = `translateX(${newPosition}px)`;
+        slideContainer.querySelector('.slide-buttons-item.active').classList.remove('active');
+        slideContainer.querySelectorAll('.slide-buttons-item')[currentSlide].classList.add('active');
     }
-}
 
-function swiperight() {
-    if (currentSlide > 0) {
-        goToSlide(currentSlide - 1);
+    function swipeleft() {
+        if (currentSlide < slides.length - 1) {
+            goToSlide(currentSlide + 1);
+        }
     }
-}
 
-function checkDirection() {
-    if (Math.abs(touchendY - touchstartY) > 100)
-        return;
-    if (touchendX < touchstartX) {
-        console.log("swiped left");
-        swipeleft();
+    function swiperight() {
+        if (currentSlide > 0) {
+            goToSlide(currentSlide - 1);
+        }
     }
-    if (touchendX > touchstartX) {
-        console.log("swiped right");
-        swiperight();
+
+    function checkDirection() {
+        if (Math.abs(touchendY - touchstartY) > 100) return;
+        if (touchendX < touchstartX) {
+            swipeleft();
+        }
+        if (touchendX > touchstartX) {
+            swiperight();
+        }
     }
-}
 
-slideContainer.addEventListener('mousedown', e => {
-    touchstartX = e.screenX;
-    touchstartY = e.screenY;
-});
+    slideContainer.addEventListener('mousedown', (e) => {
+        touchstartX = e.screenX;
+        touchstartY = e.screenY;
+    });
 
-slideContainer.addEventListener('mouseup', e => {
-    if (slideContainer.contains(e.target)) {
+    slideContainer.addEventListener('mouseup', (e) => {
+        if (slideContainer.contains(e.target)) {
         touchendX = e.screenX;
         touchendY = e.screenY;
-        checkDirection();
-    }
-});
+            checkDirection();
+        }
+    });
 
-slideContainer.addEventListener('touchstart', e => {
-    touchstartX = e.changedTouches[0].screenX;
-    touchstartY = e.changedTouches[0].screenY;
-});
+    slideContainer.addEventListener('touchstart', (e) => {
+        touchstartX = e.changedTouches[0].screenX;
+        touchstartY = e.changedTouches[0].screenY;
+    });
 
-slideContainer.addEventListener('touchend', e => {
-    if (slideContainer.contains(e.target)) {
+    slideContainer.addEventListener('touchend', (e) => {
+        if (slideContainer.contains(e.target)) {
         touchendX = e.changedTouches[0].screenX;
         touchendY = e.changedTouches[0].screenY;
-        checkDirection();
-    }
-});
+            checkDirection();
+        }
+    });
 
-slideContainer.addEventListener('wheel', (e) => {
-    if (!e.shiftKey | isSlidingWithScroll)
-        return;
-    if (e.wheelDelta < 0) {
-        isSlidingWithScroll = true;
-        swipeleft();
-    }
-    if (e.wheelDelta > 0) {
-        isSlidingWithScroll = true;
-        swiperight();
-    }
-    // set timeout
-    setTimeout(() => {
-        isSlidingWithScroll = false;
-    }, 333);
+    slideContainer.addEventListener('wheel', (e) => {
+        if (!e.shiftKey || isSlidingWithScroll) return;
+        if (e.wheelDelta < 0) {
+            isSlidingWithScroll = true;
+            swipeleft();
+        }
+        if (e.wheelDelta > 0) {
+            isSlidingWithScroll = true;
+            swiperight();
+        }
+        setTimeout(() => {
+            isSlidingWithScroll = false;
+        }, 333);
+    });
+
+    slideContainer.querySelectorAll('.slide-buttons-item').forEach((button, index) => {
+        button.addEventListener('click', () => {
+            goToSlide(index);
+        });
+    });
 });
