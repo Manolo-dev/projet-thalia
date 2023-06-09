40498cef83e1cd53497dd31af3d6a0c9434d90c0
Manolo-dev <sardo.manolo.dev@gmail.com>
2023-06-10 00:59:05 +0200

    Summary (required)

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,6 @@
 const menu = document.querySelector('.header-pages');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
 
 function openMenu() {
     menu.classList.add('open');
@@ -12,3 +14,18 @@ function toggleSubMenu(t) {
     t.parentNode.classList.toggle('open');
     t.classList.toggle('open');
 }
+
+function handleScroll() {
+    var scrollTop = main.pageYOffset || main.scrollTop;
+    console.log(scrollTop);
+    if (scrollTop === 0) {
+        header.classList.remove('scrolled');
+    } else {
+        header.classList.add('scrolled');
+    }
+    setTimeout(() => {
+        header.style.transition = 'background .5s ease-in-out';
+    }, 100);
+}
+
+handleScroll();
